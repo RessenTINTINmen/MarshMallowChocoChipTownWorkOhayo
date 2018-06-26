bead3dd65c51c089a69598e00f054df805dd4d4b
Tetsuro <happy.sadaharu@docomo.ne.jp>
2018-06-27 01:22:51 +0900

    first commit

--- tweet.py
+++ tweet.py
@@ -45,7 +45,7 @@ def puttweet_now():
         if len(s) < 140:
             break
 
-    natsu = randam.randam()*100
+    natsu = random.random()*100
     if natsu < 81 and natsu > 78:
         nastunoomoide(s)
 
